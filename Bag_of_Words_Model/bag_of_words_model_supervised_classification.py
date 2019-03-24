868f73503782a2b3ae3669ded363fd85c8f25810
mdahsanyub <md.ahsanyub@outlook.com>
2019-03-23 23:24:23 -0500

    Added random tree classifier information with some minor statements.

--- Bag_of_Words_Model/bag_of_words_model_supervised_classification.py
+++ Bag_of_Words_Model/bag_of_words_model_supervised_classification.py
@@ -10,13 +10,14 @@ __status__ = "Prototype"
 import pandas as pd
 
 #importing the data set
-dataset = pd.read_csv('sample_production_data.csv', sep='\t')
+dataset = pd.read_csv('sample_production_data_97K.csv')
+print(dataset.head())
 
 # Processing the domain names (text)
 import re
 corpus = []
 for i in range(0,97734):
-    domains = re.sub('[.]', ' ', dataset['Domain'][i]);
+    domains = re.sub('[.]', ' ', dataset['Domain'][i])
     domains = domains.lower()
     domains = domains.split()
     domains = ' '.join(domains)
@@ -24,28 +25,35 @@ for i in range(0,97734):
     
 # Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer()
+cv = CountVectorizer(max_features=90000)
 X = cv.fit_transform(corpus).toarray()
 Y = dataset.iloc[:,1].values
 
 # Spliting the dataset into the Training and Test Set
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
 # Fitting the dataset into the Training set (Implementing Decison Tree)
 from sklearn import tree
 decision_tree_classifier = tree.DecisionTreeClassifier()
 decision_tree_classifier = decision_tree_classifier.fit(X_train, Y_train)
-print("DecisionTreeClassifier")
+print("Decision Tree Classifier | Bag of Words Model")
 
 # Fitting the dataset into the Training set (Implementing Naive Bayes Tree)
 from sklearn.naive_bayes import GaussianNB
 naive_bayes_classifier = GaussianNB()
 naive_bayes_classifier.fit(X_train, Y_train)
-print("Naive Bayes")
+print("Naive Bayes | Bag of Words Model")
+
+# Fitting the dataset into the Trainig set (Implementing Random Forest)
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10000, random_state = 0)
+regressor.fit(X_train, Y_train)
+print("Random Forest | Bag of Words Model")
 
 # Predicting the Test set results
-Y_pred = decision_tree_classifier.predict(X_test)
+Y_pred = naive_bayes_classifier.predict(X_test)
+Y_pred = (Y_pred > 0.5)
 
 # Making the cufusion Matrix
 from sklearn.metrics import confusion_matrix
